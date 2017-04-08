2d1d1d0150ef43647cab4f049b5e9da9d18dbd69
Júnior Lima <jnolj@bepid.cin.ufpe.br>
2017-04-08 18:35:38 -0300

    finishing Home Screen UI

--- TransPlace/TransPlace/HomeViewController.swift
+++ TransPlace/TransPlace/HomeViewController.swift
@@ -8,10 +8,37 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
 
+class HomeViewController: UIViewController {
+    
+
+    @IBOutlet weak var searchBarButton: UIButton!
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchBarButton.layer.cornerRadius = 2.0
+        searchBarButton.layer.shadowColor = UIColor.black.cgColor
+        searchBarButton.layer.shadowOffset = CGSize(width: 0, height: 3)
+        searchBarButton.layer.shadowRadius = 5
+        searchBarButton.layer.shadowOpacity = 0.25
+        searchBarButton.setTitle("Pesquise lugares inclusivos", for: .normal)
+        searchBarButton.titleEdgeInsets = UIEdgeInsets(top: 0.0,
+                                                       left: 20.0 + (searchBarButton.imageView?.frame.width)! + 5.0,
+                                                       bottom: 0.0,
+                                                       right: 0.0)
+        searchBarButton.setTitleColor(UIColor.lightGray, for: .normal)
+        searchBarButton.imageRect(forContentRect: CGRect(x: 0,
+                                                         y: 0,
+                                                         width: (searchBarButton.imageView?.frame.width)!,
+                                                         height: (searchBarButton.imageView?.frame.height)!))
+        searchBarButton.imageEdgeInsets = UIEdgeInsets(top: 0.0, left: 20.0, bottom: 0.0, right: 0.0)
+        
+     
+        
+        
+        
 
         // Do any additional setup after loading the view.
     }
@@ -21,7 +48,7 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
 
@@ -32,4 +59,18 @@ class HomeViewController: UIViewController {
     }
     */
 
+}
+
+extension UIView {
+    func applyGradient(colours: [UIColor]) -> Void {
+        self.applyGradient(colours: colours, locations: nil)
+    }
+    
+    func applyGradient(colours: [UIColor], locations: [NSNumber]?) -> Void {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = colours.map { $0.cgColor }
+        gradient.locations = locations
+        self.layer.insertSublayer(gradient, at: 0)
+    }
 }
